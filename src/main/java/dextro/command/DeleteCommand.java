4b50ad8283eae0fedfe7d114951cbc54092bff08
AgaraNUS <e1384320@u.nus.edu>
2026-03-17 13:25:31 +0800

    Implement delete command

--- src/main/java/dextro/command/DeleteCommand.java
+++ src/main/java/dextro/command/DeleteCommand.java
@@ -1,13 +1,25 @@
 package dextro.command;
 
+import dextro.exception.CommandException;
+import dextro.model.Student;
 import dextro.model.record.StudentDatabase;
 
-public class DeleteCommand implements Command{
+public class DeleteCommand implements Command {
+    private final int index;
+
     public DeleteCommand(int index) {
+        this.index = index;
     }
 
     @Override
-    public CommandResult execute(StudentDatabase db) {
-        return null;
+    public CommandResult execute(StudentDatabase db) throws CommandException {
+        try {
+            // Subtract 1 to convert the 1-based user input to the 0-based ArrayList index
+            Student deletedStudent = db.removeStudent(index - 1);
+            return new CommandResult("Successfully deleted student:\n" + deletedStudent.toString());
+
+        } catch (IndexOutOfBoundsException e) {
+            throw new CommandException("The student at index " + index + " does not exist.");
+        }
     }
 }
