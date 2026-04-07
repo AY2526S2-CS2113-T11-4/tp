026370f6fd3e745a82352387d5dfabab0dafa170
ffluryy <kaijie.chong@u.nus.edu>
2026-04-08 02:25:42 +0800

    add module credits and more data on grades, whether they count to gpa

--- src/main/java/dextro/model/Grade.java
+++ src/main/java/dextro/model/Grade.java
@@ -1,21 +1,42 @@
 package dextro.model;
 
 public enum Grade {
-    A_PLUS("A+", 5.0),
-    A("A", 5.0),
-    A_MINUS("A-", 4.5),
-    B_PLUS("B+", 4.0),
-    B("B", 3.5),
-    B_MINUS("B-", 3.0),
-    C("C", 2.5),
-    F("F", 0.0);
+    A_PLUS("A+", 5.0, true, true),
+    A("A", 5.0, true, true),
+    A_MINUS("A-", 4.5, true, true),
+    B_PLUS("B+", 4.0, true, true),
+    B("B", 3.5, true, true),
+    B_MINUS("B-", 3.0, true, true),
+    C_PLUS("C+", 2.5, true, true),
+    C("C", 2.0, true, true),
+    D_PLUS("D+", 1.5, true, true),
+    D("D", 1.0, true, true),
+    F("F", 0.0, true, true),
+    S("S", 0.0, false, true),
+    U("U", 0.0, false, false),
+    CS("CS", 0.0, false, true),
+    CU("CU", 0.0, false, false),
+    IC("IC", 0.0, false, false),
+    IP("IP", 0.0, false, false),
+    AUD("AUD", 0.0, false, false),
+    EXE("EXE", 0.0, false, false),
+    W("W", 0.0, false, false),
+    WU("WU", 0.0, false, false),
+    DT("DT", 5.0, true, true),
+    M("M", 5.0, true, true),
+    PASS("Pass", 5.0, true, true),
+    FAIL("Fail", 5.0, true, true);
 
     private final String label;
     private final double cap;
+    private final boolean countsToGpa;
+    private final boolean countsToCompletion;
 
-    Grade(String label, double cap) {
+    Grade(String label, double cap, boolean counts, boolean countsToCompletion) {
         this.label = label;
         this.cap = cap;
+        this.countsToGpa = counts;
+        this.countsToCompletion = countsToCompletion;
     }
 
     public double getCap() {
--- src/main/java/dextro/model/Module.java
+++ src/main/java/dextro/model/Module.java
@@ -3,10 +3,18 @@ package dextro.model;
 public class Module {
     private final String code;
     private final Grade grade;
+    private final int credits;
+
+    public Module(String code, Grade grade, int credits) {
+        this.code = code;
+        this.grade = grade;
+        this.credits = credits;
+    }
 
     public Module(String code, Grade grade) {
         this.code = code;
         this.grade = grade;
+        this.credits = 4; //default 4 MCs
     }
 
     public String getCode() {
